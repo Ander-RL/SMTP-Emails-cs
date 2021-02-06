30d06ac0619c29d939bf0fb549f8858365f7bae3
Ander-RL <anderrodriguez205@gmail.com>
2021-02-06 11:41:33 +0100

    First commit

---
+++ MyFileReader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace SMTP_Emails
+{
+    class MyFileReader
+    {
+        public string from { get; set; }
+        public string to { get; set; }
+        public string password { get; set; }
+        public string subject { get; set; }
+        public string content { get; set; }
+        public string[] lines { get; set; }
+        public string[] path { get; set; }
+
+        public MyFileReader(string path) {
+            lines = System.IO.File.ReadAllLines(path);
+            from = lines[0];
+            to = lines[1];
+            password = lines[2];
+            subject = lines[3];
+            content = lines[4];
+        }
+        
+        public void Read() {
+            foreach (string line in lines) {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace SMTP_Emails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\ander\source\repos\SMTP-Emails\SMTP-Emails\MyFile.txt";
+
+            MyFileReader fr = new MyFileReader(path);
+
+            string from = fr.from;
+            string to = fr.to;
+
+            MailMessage mailMessage = new MailMessage(from, to);
+            mailMessage.Subject = fr.subject;
+            mailMessage.Body = fr.content;
+
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+
+
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = new System.Net.NetworkCredential(from, fr.password);
+            smtpClient.EnableSsl = true;
+
+            try
+            {
+                Console.WriteLine("Sending email...");
+                smtpClient.Send(mailMessage);
+                smtpClient.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception caught: {0}", ex.ToString());
+            }
+
+            Console.WriteLine("Email sent!");
+        }
+    }
+}
